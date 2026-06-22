32a1cb3af761e7f225b8a1aecbfcf1ddac5484b6
RayguekySchool <101849@selwyn.school.nz>
2026-06-22 13:33:59 +1200

    making the PS1shaders activate and deactivate

---
+++ CaligoGameProject/Assets/Scripts/RenderingShootingEffect.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class RenderingShootingEffect : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Camera targetCamera;
+    [SerializeField] private RenderTexture renderTexture;
+    [SerializeField] private GameObject targetShaderCanvas;
+
+    [Header("Settings")]
+    [SerializeField] private bool StartWithTextureOn = false;
+
+    private bool isTextureActive;
+
+    private void Start()
+    {
+        if (targetCamera == null)
+        {
+            targetCamera = Camera.main;
+        }
+
+        SetRenderTextureState(StartWithTextureOn);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ToggleRenderTexture();
+        }
+    }
+
+    public void ToggleRenderTexture()
+    {
+        SetRenderTextureState(!isTextureActive);
+    }
+
+    public void SetRenderTextureState(bool activateTexture)
+    {
+        isTextureActive = activateTexture;
+
+        if (targetCamera != null)
+        {
+            targetCamera.targetTexture = activateTexture ? renderTexture : null;
+        }
+
+        if (targetShaderCanvas != null)
+        {
+            targetShaderCanvas.SetActive(activateTexture);
+        }
+        else
+        {
+            Debug.LogWarning("CameraTextureCanvasToggler: No Canvas GameObject assigned!");
+        }
+    }
+}
